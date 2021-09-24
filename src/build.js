d10119bfe29766a5137408e6c83627c76a18ccd6
Egor <egor.osokin7@gmail.com>
2021-09-24 18:03:36 +0300

    hotfix

--- src/build.js
+++ src/build.js
@@ -3,9 +3,11 @@ const getModuleData = require("./utils/module-package");
 
 module.exports = ({ prod }) => {
   process.env.NODE_ENV = prod ? "production" : "development";
-  process.env.ENTRY = getModuleData().entrypoint;
-  const config = require("@popit/webpack-config");
+  process.env.ENTRY = getModuleData().entryPoint; // TODO: This does not work
+  let config = require("@popit/webpack-config");
+  config = { ...config, entry: getModuleData().entryPoint };
 
+  console.log("Running webpack...");
   webpack(config, (err, stats) => {
     if (err) {
       console.error(err);
